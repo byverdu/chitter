0ad86a05558783343d64418f04aeef1bd2f1a914
byverdu <byverdu@gmail.com>
2014-10-04 00:29:28 +0100

    code refactored, ready for CSS

--- app/server.rb
+++ app/server.rb
@@ -3,6 +3,7 @@ require "data_mapper"
 require "rack-flash"
 
 require_relative 'data_mapper_setup'
+require_relative 'helpers'
 
 require_relative './controllers/application'
 require_relative './controllers/user'
@@ -11,25 +12,16 @@ require_relative './controllers/session'
 
 class App < Sinatra::Base
 
-	set :views, Proc.new{ File.join(File.dirname(__FILE__), 'views') }
+	include Helpers
+
+	set :views,         Proc.new{ File.join(File.dirname(__FILE__), 'views') }
+	set :public_folder, Proc.new{ File.join(File.dirname(__FILE__), 'public') }
 
 	use Rack::Flash
 	use Rack::MethodOverride
 
 	enable :sessions
 	set    :session_secret, 'my secret'
-
-
-
-
-
-
-helpers do
-
-	def current_user
-		@current_user ||=User.get(session[:user_id]) if session[:user_id]
-	end
-end
 
 
 end
