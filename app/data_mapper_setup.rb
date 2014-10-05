8cea80ed9b9fe416ca8ffa463cc4fc6e4b588120
byverdu <byverdu@gmail.com>
2014-10-05 02:23:03 +0100

    changed setup datamapper

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,7 @@ require "data_mapper"
 
 	env = ENV["RACK_ENV"] || "development"
 
-	DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+	DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 	require_relative "./models/user"
 	require_relative "./models/chitter"
